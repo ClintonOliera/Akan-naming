c041e8ff9a8c3fbede0e450dca83f5401a614a76
clintonoliera <clintonoliera391@gmail.com>
2020-05-08 12:00:50 +0300

    added a variable and arays to list the male akan names

--- Js/script.js
+++ Js/script.js
@@ -2,5 +2,7 @@ var submitit = function () {
   var daysOfTheWeek = [
     "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
   ];
-  
+  var maleNames = [
+    "Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"
+  ];
 }
