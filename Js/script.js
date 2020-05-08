ac9cd572a08d2b9a7f8eb6962a325b02f7cffdb7
clintonoliera <clintonoliera391@gmail.com>
2020-05-08 21:18:44 +0300

    connected the form with the functionality in javascript

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,6 @@
 var submitit = function () {
   var daysOfTheWeek = [
-    "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
+    "Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
   ];
   var maleNames = [
     "Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"
@@ -18,4 +18,30 @@ var submitit = function () {
   var Display = document.getElementById("Display");
   var male = document.getElementById("Male");
   var female = document.getElementById("Female");
+
+  if (month =="" && year == "" && day == "" && name == "") {
+    alert("fill all the required inputs !");
+  }
+
+    else if (year < 0) {
+    alert( "Hey! " + name  + " please enter a valid year");
+  };
+
+  if ((month < 0 && month > 12)) {
+  alert("Hey! " + name + " please enter a valid month");
+  }
+   else if((day < 1) || (day > 31)) {
+    alert("Hey! " + name + " enter a valid day between 1 to 31");
+  }
+
+  if(male.checked && year > 0 && month > 0 && month < 12 && day > 0 && day < 32) {
+    Display.style.background = "green"
+    Display.style.color = "white"
+    Display.innerHTML = "Hey ! " + name + " you were born on " +daysOfTheWeek[find] + " your akan name is " +maleNames[find];
+  };
+  if(female.checked && year > 0 && month > 0 && month < 12 && day > 0 && day < 32) {
+    Display.style.background = "yellow"
+    Display.style.color = "white"
+    Display.innerHTML = "Hey! " + name + " you were born on " +daysOfTheWeek[find] + " your akan name is " + femaleNames[find];
+  }
 };
